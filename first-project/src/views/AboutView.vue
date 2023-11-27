84eb7890afa913884cbf55b306bf01213e6b87e8
Marcos Alves <marcosalvesjr007@gmail.com>
2023-11-27 14:58:55 +0000

    feat: create_navbar + page_projects

---
+++ first-project/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>Conecte-se comigo!</h1>
+    <a>Github.com</a>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ first-project/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1 class="green">Veja mais projetos</h1>
+    <a>Angular</a>
+    <a>Spotify</a>
+    <a>Portifolio</a>
+    <a>GrowthStage</a>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+</style>
